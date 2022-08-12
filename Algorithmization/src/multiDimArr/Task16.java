b7e22b08ee175eaddb299b5c05b5b30b37b6ede8
Golovolastik <yankovich.aliaksei@gmail.com>
2022-08-12 22:34:15 +0300

    Next time =(

--- Algorithmization/src/multiDimArr/Task16.java
+++ Algorithmization/src/multiDimArr/Task16.java
@@ -4,28 +4,36 @@ package multiDimArr;
 // !!! Работает только с нечетными значениями n.
 public class Task16 {
     public static void main(String[] args){
-        int n = 4;
+        int n = 3;
         int m = n*(n*n+1)/2; // Сумма элементов в каждом ряду, столбце и диагоналях
         int[][] array = new int[n][n];
         System.out.println(m);
         int i = n/2;
         int j = n-1;
         int number = 1;
+        array[i][j] = number;
         while (number <= n*n){
-            array[i][j] = number;
-            i -= 1;
-            j += 1;
+            number++;
             if (i==-1 && j==n){
                 i = 0;
                 j = n-2;
+            } else {
+                if (i-1 < 0) {
+                    i = n-1;
+                }
+                if (j+1 > n-1) {
+                    j = 0;
+                }
             }
-            i = i < 0 ? n-1 : i;
-            j = j > n-1 ? 0 : j;
+
             if (array[i][j] != 0) {
                 i += 1;
                 j -= 2;
             }
-            number++;
+            array[i][j] = number;
+            i -= 1;
+            j += 1;
+
         }
         MultiDimArr.printTwoDInt(array, n, n);
 
