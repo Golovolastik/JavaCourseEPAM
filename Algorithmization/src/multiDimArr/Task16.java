71014d78268564a50cdb7dad9ec1538e3ccbf2e3
Golovolastik <yankovich.aliaksei@gmail.com>
2022-08-14 12:34:07 +0300

    Task 16 complete

--- Algorithmization/src/multiDimArr/Task16.java
+++ Algorithmization/src/multiDimArr/Task16.java
@@ -4,38 +4,38 @@ package multiDimArr;
 // !!! Работает только с нечетными значениями n.
 public class Task16 {
     public static void main(String[] args){
-        int n = 3;
-        int m = n*(n*n+1)/2; // Сумма элементов в каждом ряду, столбце и диагоналях
+        int n = 7;
+        if (n%2 == 0) {
+            System.exit(0);
+        }
         int[][] array = new int[n][n];
-        System.out.println(m);
         int i = n/2;
         int j = n-1;
-        int number = 1;
-        array[i][j] = number;
-        while (number <= n*n){
-            number++;
-            if (i==-1 && j==n){
-                i = 0;
-                j = n-2;
-            } else {
-                if (i-1 < 0) {
-                    i = n-1;
-                }
-                if (j+1 > n-1) {
-                    j = 0;
-                }
-            }
-
-            if (array[i][j] != 0) {
-                i += 1;
-                j -= 2;
-            }
+        for (int number=1; number<=n*n; number++){
             array[i][j] = number;
             i -= 1;
             j += 1;
-
+            if (i==-1 && j==n){
+                i = 0;
+                j = n-2;
+            }
+            if (i==-1){
+                i = n-1;
+            }
+            if (j==n) {
+                j = 0;
+            }
+            if (array[i][j] != 0){
+                i += 1;
+                j -= 2;
+            }
         }
+        int m = n*(n*n+1)/2; // Сумма элементов в каждом ряду, столбце и диагоналях
+        System.out.printf("Magic square of size %d\n" +
+                        "Sum in each row & column: %d\n", n, m);
         MultiDimArr.printTwoDInt(array, n, n);
+
+
 
     }
 }
