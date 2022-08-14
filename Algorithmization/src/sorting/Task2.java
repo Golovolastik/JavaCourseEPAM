679cca08d31a774c24db1ac601028f3f3eac864e
Golovolastik <yankovich.aliaksei@gmail.com>
2022-08-14 22:00:31 +0300

    still not

--- Algorithmization/src/sorting/Task2.java
+++ Algorithmization/src/sorting/Task2.java
@@ -1,15 +1,26 @@
 package sorting;
 
+import oneDimArr.OneDimArr;
+
 // Даны две возрастающие последовательности.
 // Образовать из ниx возрастающую последовательность
 public class Task2 {
     public static void main(String[] args){
-        int[] array1 = {1, 3, 5, 7};
-        int[] array2 = {2, 4, 6, 8, 10};
+        int[] array1 = {2, 4, 6, 8, 10};
+        int[] array2 = {1, 3, 5, 7};
         int[] result = new int[array1.length + array2.length];
         for (int i=0, j=0; i<array1.length || j<array2.length; i++, j++){
-            i = i==array1.length ? --i : i;
-            j = j==array2.length ? --j : j;
+            if (i==array1.length){
+                for (int k=j; k<array2.length; k++) {
+                    result[i+k] = array2[k];
+                }
+                continue;
+            } else if (i==array2.length) {
+                for (int k=i; k<array1.length; k++) {
+                    result[i+k] = array1[k];
+                }
+                continue;
+            }
             if (array1[i] < array2[j]) {
                 result[i+j] = array1[i];
                 j--;
@@ -18,5 +29,6 @@ public class Task2 {
                 i--;
             }
         }
+        OneDimArr.printInt(result);
     }
 }
