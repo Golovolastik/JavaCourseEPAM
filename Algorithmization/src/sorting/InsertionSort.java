f55bfc0e7bdbc3c340c5dec5aeff297769d07ade
Golovolastik <yankovich.aliaksei@gmail.com>
2022-08-16 18:04:48 +0300

    binnary search function

--- Algorithmization/src/sorting/InsertionSort.java
+++ Algorithmization/src/sorting/InsertionSort.java
@@ -3,6 +3,29 @@ package sorting;
 import oneDimArr.OneDimArr;
 
 public class InsertionSort {
+    public static int findPosition(int[] arr, int start, int end, int number){
+        int idx = 0;
+        if (start == end) {
+            idx = number > start ? start + 1 : start;
+        }
+        int high = end;
+        int low = start;
+        int counter = 0;
+        while (/*high >= low &&*/ high - low != 1) {
+            counter++;
+            idx = (high + low) / 2;
+            if (arr[idx] == number) {
+                System.out.println(counter);
+                return idx;
+                } else if (arr[idx] > number) {
+                    high = idx;
+                } else {
+                low = idx;
+            }
+        }
+        System.out.println(counter);
+        return idx;
+    }
     public static void slide_sort(int[] arr){
         for (int i=1; i<arr.length; i++) {
             int key = arr[i];
@@ -24,6 +47,8 @@ public class InsertionSort {
     public static void main(String[] args) {
         int[] array = {9, 8, 7, 6, 5, 4, 3, 2, 1};
         InsertionSort.slide_sort(array);
-        OneDimArr.printInt(array);
+        //OneDimArr.printInt(array);
+        int index = findPosition(array, 0, array.length-1, 6);
+        System.out.println(index);
     }
 }
