2c8b82081c4a988b13b100ab4a88f40cb6b93fec
Golovolastik <yankovich.aliaksei@gmail.com>
2022-08-16 15:32:39 +0300

    Slide sorting

---
+++ Algorithmization/src/sorting/InsertionSort.java
@@ -0,0 +1,23 @@
+package sorting;
+
+import oneDimArr.OneDimArr;
+
+public class InsertionSort {
+    public static void slide_sort(int[] arr){
+        for (int i=1; i<arr.length; i++) {
+            int key = arr[i];
+            int j = i - 1;
+            while (j>=0 && arr[j]>key) {
+                arr[j+1] = arr[j];
+                j -= 1;
+            }
+            arr[j+1] = key;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] array = {9, 8, 7, 6, 5, 4, 3, 2, 1};
+        InsertionSort.slide_sort(array);
+        OneDimArr.printInt(array);
+    }
+}
