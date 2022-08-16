136ed271d0580677a0e67e0707f5a40e3406581e
Golovolastik <yankovich.aliaksei@gmail.com>
2022-08-16 15:42:49 +0300

    todo list

---
+++ Algorithmization/src/multiDimArr/Task12.java
@@ -0,0 +1,4 @@
+package multiDimArr;
+//todo complete task 12 and 13 sorting matrix rows and columns
+public class Task12 {
+}
--- Algorithmization/src/sorting/InsertionSort.java
+++ Algorithmization/src/sorting/InsertionSort.java
@@ -15,6 +15,12 @@ public class InsertionSort {
         }
     }
 
+    public static void sort(int[] arr){
+          //todo fulfill the conditions:
+          // - binary search method
+          // - insertion with binary search method
+    }
+
     public static void main(String[] args) {
         int[] array = {9, 8, 7, 6, 5, 4, 3, 2, 1};
         InsertionSort.slide_sort(array);
