f55eace4dad9e648f54053ef162d0c3ad837ccb8
Golovolastik <yankovich.aliaksei@gmail.com>
2022-08-18 17:03:23 +0300

    dirty but working

--- Algorithmization/src/sorting/InsertionSort.java
+++ Algorithmization/src/sorting/InsertionSort.java
@@ -1,9 +1,10 @@
 package sorting;
 
 import oneDimArr.OneDimArr;
+import org.jetbrains.annotations.NotNull;
 
 public class InsertionSort {
-    public static int findPosition(int[] arr, int start, int end, int number){
+    public static int findPosition(int[] arr, int start, int end, int number) {
         int idx = 0;
         if (start == end) {
             idx = number > start ? start + 1 : start;
@@ -11,22 +12,30 @@ public class InsertionSort {
         int high = end;
         int low = start;
         int counter = 0;
-        while (/*high >= low &&*/ high - low != 1) {
+        while (high - low != 1) {
             counter++;
             idx = (high + low) / 2;
             if (arr[idx] == number) {
-                System.out.println(counter);
+                //System.out.println(counter);
                 return idx;
-                } else if (arr[idx] > number) {
-                    high = idx;
-                } else {
+            } else if (arr[idx] > number) {
+                high = idx;
+            } else {
                 low = idx;
             }
         }
-        System.out.println(counter);
+        //System.out.println(counter);
+        if (idx > 0) {
+            if (arr[idx - 1] > number) {
+                return --idx;
+            } else if (arr[idx] < number) {
+                return ++idx;
+            }
+        }
         return idx;
     }
-    public static void slide_sort(int[] arr){
+
+    public static void slide_sort(int [] arr){
         for (int i=1; i<arr.length; i++) {
             int key = arr[i];
             int j = i - 1;
@@ -38,17 +47,33 @@ public class InsertionSort {
         }
     }
 
+    //todo fulfill the conditions:
+    // - binary search method
+    // - insertion with binary search method
+
     public static void sort(int[] arr){
-          //todo fulfill the conditions:
-          // - binary search method
-          // - insertion with binary search method
+        for (int i=1; i < arr.length; i++){
+            int key = arr[i];
+            int idx = findPosition(arr, 0, i, key);
+            //System.out.println(idx);
+            System.arraycopy(arr, idx, arr, idx+1, i-idx);
+            arr[idx] = key;
+        }
     }
 
     public static void main(String[] args) {
-        int[] array = {9, 8, 7, 6, 5, 4, 3, 2, 1};
-        InsertionSort.slide_sort(array);
+        int[] array = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+        int[] testArr = new int [20];
+        for (int i=0; i< testArr.length; i++){
+            testArr[i] = (int) (Math.random() * 20);
+        }
+        OneDimArr.printInt(testArr);
+        //InsertionSort.slide_sort(array);
         //OneDimArr.printInt(array);
-        int index = findPosition(array, 0, array.length-1, 6);
-        System.out.println(index);
+        //System.out.println(testArr.length);
+        //int index = findPosition(testArr, 0, testArr.length-1, 2);
+        //System.out.println(index);
+        sort(testArr);
+        OneDimArr.printInt(testArr);
     }
 }
