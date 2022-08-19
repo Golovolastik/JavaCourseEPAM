9dfa445f6931452ff35100836ee9f721d6194f3f
Golovolastik <yankovich.aliaksei@gmail.com>
2022-08-19 20:55:11 +0300

    task2 complete

--- Algorithmization/src/dec/Task2.java
+++ Algorithmization/src/dec/Task2.java
@@ -3,6 +3,12 @@ package dec;
 // Функция для нахождения НОД 4 чисел
 public class Task2 {
     public static void main(String [] args){
+        int a = 20;
+        int b = 50;
+        int c = 120;
+        int d = 140;
+        int result = Factors.GCF(Factors.GCF(Factors.GCF(a, b), c), d);
+        System.out.println(result);
 
     }
 }
