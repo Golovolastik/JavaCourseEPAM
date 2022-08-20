151a94826f9511883e77698f3fda1ea98c076d2e
Golovolastik <yankovich.aliaksei@gmail.com>
2022-08-20 12:49:30 +0300

    nor ready

---
+++ Algorithmization/src/dec/Task4.java
@@ -0,0 +1,11 @@
+package dec;
+
+
+import oneDimArr.OneDimArr;
+
+// На плоскости заданы своими координатами n точек.
+// Написать методы, опреляющие, между какими из пар точек самое большое расстояние
+public class Task4 {
+    OneDimArr oneDim = new OneDimArr();
+    int[][] array = {{1, 4, -2, 0, 1}, {-3, 8, -2, 1, 4}};
+}
