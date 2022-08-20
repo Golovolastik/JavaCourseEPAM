7be22e78ee1cda0b01cc57e0020811afb35fd5bb
Golovolastik <yankovich.aliaksei@gmail.com>
2022-08-20 16:39:47 +0300

    make methods public

--- Algorithmization/src/multiDimArr/MultiDimArr.java
+++ Algorithmization/src/multiDimArr/MultiDimArr.java
@@ -3,7 +3,7 @@ package multiDimArr;
 import java.util.Random;
 
 public class MultiDimArr {
-    static void initTwoDInt(int[][] arr, int rowNumber, int columnNumber){
+    public static void initTwoDInt(int[][] arr, int rowNumber, int columnNumber){
         Random random = new Random();
         for (int i=0; i<rowNumber; i++){
             for (int j=0; j<columnNumber; j++){
@@ -12,7 +12,7 @@ public class MultiDimArr {
         }
     }
 
-    static void printTwoDInt(int[][] arr, int rowNumber, int columnNumber){
+    public static void printTwoDInt(int[][] arr, int rowNumber, int columnNumber){
         for (int i=0; i<rowNumber; i++){
             for (int j=0; j<columnNumber; j++){
                 System.out.printf("%4d ", arr[i][j]);
@@ -21,7 +21,7 @@ public class MultiDimArr {
         }
     }
 
-    static void printTwoDDouble(double[][] arr, int rowNumber, int columnNumber){
+    public static void printTwoDDouble(double[][] arr, int rowNumber, int columnNumber){
         for (int i=0; i<rowNumber; i++){
             for (int j=0; j<columnNumber; j++){
                 System.out.printf("%7.3f ", arr[i][j]);
