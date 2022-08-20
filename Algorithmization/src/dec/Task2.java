e3f7c2c2604a7b7c321226bcb6816a4383c34faa
Golovolastik <yankovich.aliaksei@gmail.com>
2022-08-20 12:07:24 +0300

    task2 complete

--- Algorithmization/src/dec/Task2.java
+++ Algorithmization/src/dec/Task2.java
@@ -2,12 +2,31 @@ package dec;
 
 // Функция для нахождения НОД 4 чисел
 public class Task2 {
+    public static int fourGCF(int a, int b, int c, int d){
+        int result = Factors.GCF(Factors.GCF(Factors.GCF(a, b), c), d);
+
+        return result;
+    }
+
+    public static int endlessGCF(int[] array){
+        int i = 1;
+        while (i < array.length){
+            array[i] = Factors.GCF(array[i-1], array[i]);
+            i++;
+        }
+        int result = array[i-1];
+
+        return result;
+    }
     public static void main(String [] args){
         int a = 20;
         int b = 50;
         int c = 120;
         int d = 140;
-        int result = Factors.GCF(Factors.GCF(Factors.GCF(a, b), c), d);
+        int result = fourGCF(a, b, c, d);
+        System.out.println(result);
+        int[] array = {a, b, c, d, d, c, b, a};
+        result = endlessGCF(array);
         System.out.println(result);
 
     }
