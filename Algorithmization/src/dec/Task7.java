917131320f03dcb8fd32338ff3662c5621069423
Golovolastik <yankovich.aliaksei@gmail.com>
2022-08-21 14:54:24 +0300

    task 7 complete

---
+++ Algorithmization/src/dec/Task7.java
@@ -0,0 +1,28 @@
+package dec;
+
+// Написать методы для вычисления факториалов всех нечетных чисел от 1 до 9
+public class Task7 {
+    public static int factorial(int n){
+        int result = 1;
+        for (int i=2; i<=n; i++){
+            result *= i;
+        }
+
+        return result;
+    }
+
+    public static int[] oddFactorials(int n){
+        int[] array = new int[n/2];
+        int count=0;
+        for (int i=1; i<=n; i+=2){
+            array[count] = factorial(i);
+            System.out.printf("Factorial of %d is %d\n", i, array[count]);
+        }
+
+        return array;
+    }
+
+    public static void main(String[] args){
+        int[] result = oddFactorials(9);
+    }
+}
