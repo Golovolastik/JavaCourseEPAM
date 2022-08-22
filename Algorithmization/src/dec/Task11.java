a47340c91e1e886c6924315539555f0d25ba4201
Golovolastik <yankovich.aliaksei@gmail.com>
2022-08-22 10:37:49 +0300

    complete

---
+++ Algorithmization/src/dec/Task11.java
@@ -0,0 +1,21 @@
+package dec;
+
+// Написать метод, определяющий, в каком из данных двух чисел больше цифр
+public class Task11 {
+    static void compare(int x, int y){
+        String a = Integer.toString(x);
+        String b = Integer.toString(y);
+        if (a.length() > b.length()){
+            System.out.printf("%d is greater than %d", x, y);
+        } else if (a.length() < b.length()) {
+            System.out.printf("%d is greater than %d", y, x);
+        } else {
+            System.out.printf("%d and %d are equal", x, y);
+        }
+    }
+    public static void main(String[] args){
+        int x = 1024;
+        int y = 256;
+        compare(x, y);
+    }
+}
