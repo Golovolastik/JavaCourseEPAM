293aadebc56c9f2fe626e93fdb347e93168cac17
Golovolastik <yankovich.aliaksei@gmail.com>
2022-08-30 12:49:15 +0300

    complete

--- Algorithmization/src/dec/Task13.java
+++ Algorithmization/src/dec/Task13.java
@@ -4,19 +4,22 @@ import oneDimArr.OneDimArr;
 
 // Найти и напечатать все пары близнецов из отрезка [n, 2n]
 public class Task13 {
-    static int[] initArray(int n){
-        int[] array = new int[n+1];
-        for (int i=0; i<array.length; i++){
-            array[i] = i+n;
-        }
-        return array;
-    }
     static void findTwins(int n){
-        int[] array = new int[n];
+        int[] array = Prime.findPrimes(2*n);
+        printTwins(array, n);
+    }
+    static void printTwins(int[] array, int n) {
+        for (int i=0; i<array.length-2; i++){
+            if (array[i]<n){
+                continue;
+            }
+            if (array[i] + 2 == array[i+1]){
+                System.out.printf("(%d, %d) ", array[i], array[i+1]);
+            }
+        }
     }
     public static void main(String[] args){
-        int n = 30;
-        int[] array = initArray(n);
-        OneDimArr.printInt(array);
+        int n = 500;
+        findTwins(n);
     }
 }
