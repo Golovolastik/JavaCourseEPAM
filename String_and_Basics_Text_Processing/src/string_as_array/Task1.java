70f74fa62f2a20f2015d744dd0505a5408898010
Golovolastik <yankovich.aliaksei@gmail.com>
2022-08-31 19:12:59 +0300

    to lowercase

--- String_and_Basics_Text_Processing/src/string_as_array/Task1.java
+++ String_and_Basics_Text_Processing/src/string_as_array/Task1.java
@@ -2,19 +2,23 @@ package string_as_array;
 
 import java.util.ArrayList;
 
+import static java.lang.Character.isUpperCase;
+import static java.lang.Character.toLowerCase;
+
 public class Task1 {
     static void camelCase_to_snake_case(char[] name){
-        ArrayList<char> result = new ArrayList<char>();
+        ArrayList<Character> result = new ArrayList<Character>();
         for (int i=0; i<name.length; i++){
             if (isUpperCase(name[i])){
-                toLowerCase(name[i]);
+                name[i] = toLowerCase(name[i]);
             }
         }
 
         //return result;
     }
     public static void main(String[] args) {
-        char[] test = {'h', 'e', 'l', 'l', 'o'};
+        char[] test = {'H', 'e', 'L', 'l', 'O'};
+        camelCase_to_snake_case(test);
         for (char i:
              test) {
             System.out.print(i + " ");
